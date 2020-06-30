d1101f206981ca96569f1db78df0d40d060c99ec
Antao Almada <antao.almada@gmail.com>
2020-07-01 00:04:57 +0100

    Add support for ArraySegment<>

--- NetFabric.Hyperlinq/Partitioning/Skip/Skip.Array.cs
+++ NetFabric.Hyperlinq/Partitioning/Skip/Skip.Array.cs
@@ -6,13 +6,27 @@ namespace NetFabric.Hyperlinq
     public static partial class ArrayExtensions
     {
 #if SPAN_SUPPORTED
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Memory<TSource> Skip<TSource>(this TSource[] source, int count)
             => Skip(source.AsMemory(), count);
+
 #else
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static SkipTakeEnumerable<TSource> Skip<TSource>(this TSource[] source, int count)
-            => SkipTake(source, count, source.Length);
+        public static ArraySegment<TSource> Skip<TSource>(this TSource[] source, int count)
+        {
+            var (skipCount, takeCount) = Utils.Skip(source.Length, count);
+            return new ArraySegment<TSource>(source, skipCount, takeCount);
+        }
+
 #endif
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ArraySegment<TSource> Skip<TSource>(this in ArraySegment<TSource> source, int count)
+        {
+            var (skipCount, takeCount) = Utils.Skip(source.Count, count);
+            return new ArraySegment<TSource>(source.Array, skipCount, takeCount);
+        }
     }
 }
